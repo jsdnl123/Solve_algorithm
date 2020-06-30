d4355d58fb8173bd29bc9debdf7300713878f729
위진세 <jsdnl123@lgcns.com>
2020-06-30 14:05:43 +0900

    체육복 개인의견 추가

--- Coding_Lv1_체육복/Program.cs
+++ Coding_Lv1_체육복/Program.cs
@@ -6,7 +6,23 @@ namespace Coding_Lv1_체육복
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solution sol = new Solution();
+            
+            //반 인원수
+            int n = 5;
+
+            //잃어버린 사람
+            int[] lost = { 2, 3 };
+            //여유분 갖고 있는사람
+            int[] reserve = { 3, 4 };
+            int answer = sol.solution(n, lost, reserve);
+            // answer =4;
+            // 위 상황에서 2번 학생이 3번 체육복을, 3번이 4번 것을 빌리면 다 입을 수 있으나,
+            // Test Case 확인 시 답은 4임.
+            // 애초에 저런 Test Case를 준다는 게 현실적으로 맞지 않지만,
+            // 여기서 lost이면서 reserve인 학생은 본인이 본인 여유분을 입는 것으로 보임.
+
+            Console.WriteLine(answer);
         }
     }
 
